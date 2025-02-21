e791e98140a0ce9ccafddcecfef3c667807d8645
tfermoso <tomas.fermoso@gmail.com>
2025-02-21 15:54:29 +0100

    integramso bootstrap

--- app/models/Film.php
+++ app/models/Film.php
@@ -4,7 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 class Film extends Model{
     protected $table="film";
     protected $primaryKey = 'film_id';
-
+    public function actors() {
+        return $this->belongsToMany(Actor::class, 'film_actor', 'film_id', 'actor_id');
+    }
 }
 
 ?>
